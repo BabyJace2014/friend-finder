5b1c6d78185fa0823f5247af54e6fd4128940100
BabyJace2014 <35673340+BabyJace2014@users.noreply.github.com>
2018-08-14 12:25:51 -0400

    Ready For Testing

---
+++ app/data/friends.js
@@ -0,0 +1,22 @@
+// Array that holds all the 'friends' that have taken the survey
+// Initialize startting with Dummy Data
+var friendList = [
+    {
+        name: 'Bob Builder',
+        profilePic: 'https://drive.google.com/open?id=1LRJ43HQidSZfxTRKfkYiOPJWi5SrGSm_',
+        scores: [
+            "4",
+            "5",
+            "5",
+            "2",
+            "4",
+            "5",
+            "4",
+            "4",
+            "3",
+            "4"
+        ]
+    }
+]
+
+module.exports = friendList;
