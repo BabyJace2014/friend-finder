448d8a5bbdd2de86c955a16105bcb7414d0b1f4d
BabyJace2014 <35673340+BabyJace2014@users.noreply.github.com>
2018-08-15 10:03:00 -0400

    Minor Changes

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,22 +1,37 @@
 // Array that holds all the 'friends' that have taken the survey
 // Initialize startting with Dummy Data
+
 var friendList = [
     {
-        name: 'Bob Builder',
-        profilePic: 'https://drive.google.com/open?id=1LRJ43HQidSZfxTRKfkYiOPJWi5SrGSm_',
-        scores: [
-            "4",
-            "5",
-            "5",
-            "2",
-            "4",
-            "5",
-            "4",
-            "4",
-            "3",
-            "4"
-        ]
+        name: "Patrick",
+        profilePic: "https://i.pinimg.com/736x/ee/85/08/ee850842e68cfcf6e3943c048f45c6d1--patrick-stewart-patrick-obrian.jpg",
+        scores: [ 5, 4, 3, 2, 1, 5, 4, 3, 2, 1 ]
+    }, 
+    {
+        name: "Jim",
+        photo: "https://i.pinimg.com/736x/2a/23/0b/2a230b4972b3d685f87a81c9bfcfeabe--funny-guys-funny-people.jpg",
+        scores: [ 1, 2, 3, 4, 5, 1, 2, 3, 4, 5 ]
+    }, 
+    {
+        name: "Don",
+        photo: "http://img.timeinc.net/time/daily/2007/0705/a_br10qcheadle_0528.jpg?w=240",
+        scores: [ 5, 5, 4, 4, 3, 3, 2, 2, 1, 1 ]
+    }, 
+    {
+        name: "Ben",
+        photo: "https://i.pinimg.com/originals/be/f2/8d/bef28d8311ef02dc6a6456b5298b9709.jpg",
+        scores: [ 1, 1, 2, 2, 3, 3, 4, 4, 5, 5 ]
+    }, 
+    {
+        name: "Samuel",
+        photo: "http://www.eurweb.com/wp-content/uploads/2011/11/samuel_l_jackson2011-bw-turtleneck-big-ver-upper.jpg",
+        scores: [ 5, 4, 3, 2, 1, 1, 2, 3, 4, 5 ]
+    }, 
+    {
+        name: "Simon",
+        photo: "https://i.pinimg.com/736x/36/80/f0/3680f0a9ca2e2a7049d7c99c955d1354--face-p-sad-face.jpg",
+        scores: [ 1, 2, 3, 4, 5, 5, 4, 3, 2, 1 ]
     }
-]
+];
 
 module.exports = friendList;
